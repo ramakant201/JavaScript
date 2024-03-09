12e74971132156194e3c53bad8d6d619f7074e59
ramakant201 <53092424+ramakant201@users.noreply.github.com>
2024-03-09 17:29:18 +0530

    Update DupliacteCharCount.js

--- DupliacteCharCount.js
+++ DupliacteCharCount.js
@@ -5,3 +5,13 @@ for(let i=0; i< noramalString.length; i++){
     charCount[noramalString[i]] = (charCount[noramalString[i]] || 0) + 1;
 }
 console.log(charCount);
+
+// Count All Duplicate Characters in a array 
+
+var arr=[3,4,5,6,5,4,3,7,8,9,3,2,1,4,5,6];
+
+var res =arr.reduce(function(acc,cur){
+acc[cur]=(acc[cur]||0)+1;
+return acc;
+},{});
+console.log(res);
